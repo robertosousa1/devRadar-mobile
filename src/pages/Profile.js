a1556ecd3ede0c8d2c63f1acd1433a09dd89e492
Roberto <roberto_junior_sousa@hotmail.com>
2020-01-16 16:51:23 -0300

    criando webview para perfil do github

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import { View } from 'react-native';
+import { WebView } from 'react-native-webview';
 
-export default function Profile() {
+export default function Profile({ navigation }) {
+  const githubUsername = navigation.getParam('github_username');
+
   return (
-    <View />
+    <WebView style={{ flex: 1 }} source={{ uri: `https://github.com/${githubUsername}` }} />
   );
 }
